e91b31169af72793504912513a5b82401a7cdd2c
Samarth Patel <146717917+s-p-1@users.noreply.github.com>
2025-01-22 21:53:11 +0530

    Update hello.c
    Made the necessary initialization and memory allocation

--- hello.c
+++ hello.c
@@ -1,11 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int min(int a, int b) {
+    return (a < b) ? a : b;
+}
 //a file that runs until q 
-main(){
-    char c;
-    scanf("%c", &c);
-    while (c != 'q'){
+void display (int ** mysheet, int R, int C, int x, int y){
+    for (int i = x; i < min(R, x+10); i++){
+        for (int j = y; j < min(C, y+10); j++){
+            printf("%d ", mysheet[i][j]);
+        }
+        printf("\n");
+
+    }
+    fflush(stdout);
+}
+
+
+int main(int argc, char *argv[]){
+    int R = atoi (argv[1]);
+    int C = atoi (argv[2]);
+    int ** mysheet = (int **)malloc(R * sizeof(int *));
+    for (int i = 0; i < R; i++){
+        mysheet[i] = (int *)calloc(C * sizeof(int), 0);
+    }
+    int x, y;
+    x=0;
+    y=0;
+    display(mysheet, R, C, x, y);
+
+    char s[100];
+    scanf("%s", &s);
+    while (s[0] != 'q'){
         printf("Hello, World!\n");
-        scanf("%c", &c);
+        scanf("%s", &s);
     }
     return 0;
 }
