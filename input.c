5000e884e2d9f1a724e8a1b36584e9aa18000135
priyanka sar <146313569+2023cs50578@users.noreply.github.com>
2025-01-24 11:02:21 +0530

    Create input.c
    added the file to handle the input

---
+++ input.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int stringcomp(const char s1[20], const char s2[20], char myc){
+        int flag =1;
+        for (int i =0; i<20; ++i){
+                if (s1[i]==myc){
+                        if (s2[i]!=myc) flag=0;
+                        break;
+                }
+                if (s1[i]!=s2[i]){
+                        flag=0;
+                        break;
+                }
+        }
+        return flag;
+}
+
+
+
+
+int mycomp(char s[20]){
+        if (stringcomp("w", s, '\0')==1) return 1;
+        if (stringcomp("a", s, '\0')==1) return 2;
+        if (stringcomp("s", s, '\0')==1) return 3;
+        if (stringcomp("d", s, '\0')==1) return 4;
+        if (stringcomp("q", s, '\0')==1) return 5;
+        if (stringcomp("disable_output", s, '\0')==1) return 6;
+        if (stringcomp("enable_output", s, '\0')==1) return 7;
+        if (stringcomp("scroll_to ", s, ' ')==1){
+                printf("call the print function to the required cell here\n");
+                return 8;
+        }
+        printf("give correct input\n");
+        return -1;
+}
