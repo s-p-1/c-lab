b817a0726f3f0b068715150d31fd6d47faa71882
priyanka sar <146313569+2023cs50578@users.noreply.github.com>
2025-01-29 16:35:24 +0000

    Made the struct of cell to be used across files

--- helper.h
+++ helper.h
@@ -2,5 +2,16 @@
 #define HELPER_H
 
 int get_line();
+typedef struct {
+    int value; //initially 0
+    char operation; //0 initially and for value/sleep, 
+    //1 for add, 2 for subtract, 3 for multiply, 4 for divide
+    //5 for min, 6 for max, 7 for avg, 8 for sum, 9 for stdev
+    short row1;
+    short col1;
+    short row2;
+    short col2;
+    //above four will be initialized to -1;
+} cell; 
 
 #endif
