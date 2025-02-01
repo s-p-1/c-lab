85a57e43456f4c4f0e2f08de9235013df16cc4c9
Samarth Patel <146717917+s-p-1@users.noreply.github.com>
2025-02-01 20:50:39 +0000

    nothing

--- graph.c
+++ graph.c
@@ -1,25 +1,50 @@
 #include "helper.h"
 
+void dfs (int address){
+    AVLNode* root = mysheet[address%1000-1][address/1000-1].cell_avl;
+    int myl []= inorderTraversal(root);
+    while (*myl!=-1){
+        int addr1= *myl;
+        mysheet[addr1%1000-1][addr1/1000-1].count += 1;
+        printf("%d ", *myl);
+        myl++;
+    }
+
+}
+
+
+
 void prograph(int address){
-    int *queue = (int *)malloc(5 * sizeof(int));
+    int size = 5;
+    int *queue = (int *)malloc(size * sizeof(int));
+    
+    queue[0] = address;
     int front = 0;
     int rear = 1;
+    queue[0] = address;
     AVLNode* root = mysheet[address%1000-1][address/1000-1].cell_avl;
     int size = 5;
     while (rear != front) {
-        int arr[5]; // this array is from the avl tree code
+        int address = queue[front];
+        AVLNode* root = mysheet[address%1000-1][address/1000-1].cell_avl;
+        int* myl= inorderTraversal(root);
         //this array contains adddress of the cells
-        if (front == -1) {
-            front = 0;
+
+        while (*myl!=-1){
+            int addr1= *myl;
+            //update the value here
+            mysheet[addr1%1000-1][addr1/1000-1].count -= 1;
+            if (mysheet[addr1%1000-1][addr1/1000-1].count == 0){
+                queue[rear] = addr1;
+                rear++;
+            }
+            myl++;
         }
+
         if (rear == size) {
             printf("Queue is full\n");
             break;
         }
-        int x;
-        scanf("%d", &x);
-        queue[rear] = x;
-        rear++;
     }
 }
 
