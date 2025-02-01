d6597edd4f1f408babb0b96a00d847c21fb45890
Samarth Patel <146717917+s-p-1@users.noreply.github.com>
2025-02-01 13:09:47 +0000

    small changes

--- asscop.c
+++ asscop.c
@@ -48,54 +48,64 @@ rowcol cell_handler(char *cell){
     // return 0;
 }
 
-char parser(char input[]){
+char parser(char* input[]){
     char *cell;
+    char op = '-1';
     cell=strtok(input, "=");
     if (cell_handler(cell).row==-1){
-        printf("Invalid First cell\n");
+        op = '1';
         return '1';
     }
 
     char *exp;
     char *cell1;
     char *cell2;
-    if (cell_handler(cell1).row==-1){
-        printf("Invalid Second cell\n");
-        return '2';
-    }
-    if (cell_handler(cell2).row==-1){
-        printf("Invalid Third cell\n");
-        return '3';
-    }
+    
     char *range;
     exp=strtok(NULL, "=");
     // printf("%s\n", exp);
     char *arr[200];
-    if (strpbrk(exp, "+-*/")!=NULL){
+    if (cell_handler(exp).row!=-1){
+        op = 'c';
+        
+        return 'c';
+    }
+    else if (strpbrk(exp, "+-*/")!=NULL){
         if (strpbrk(exp, "+")!=NULL){
             cell1=strtok(exp, "+");
             cell2=strtok(NULL, "+");
             printf("%s %s\n", cell1, cell2);
-            return '+';
+            op = '+';
         }
         else if (strpbrk(exp, "-")!=NULL){
             cell1=strtok(exp, "-");
             cell2=strtok(NULL, "-");
-            return '-';
+            op = '-';
         }
         else if (strpbrk(exp, "*")!=NULL){
             cell1=strtok(exp, "*");
             cell2=strtok(NULL, "*");
-            return '*';
+            op = '*';
         }
         else if (strpbrk(exp, "/")!=NULL){
             cell1=strtok(exp, "/");
             cell2=strtok(NULL, "/");
-            return '/';
+            op = '/';
         }
         else{
+            op = 'q';
             return 'q';
         }
+        if (cell_handler(cell1).row==-1){
+            op = '2';
+            return '2';
+        }
+        if (cell_handler(cell2).row==-1){
+            op = '3';
+            return '3';
+        }
+        // handle the operations here
+        return 's';
         printf("%s %s\n", cell1, cell2);
     }
     else if (strpbrk(exp, "MINMAXAVGSUMSTDEVSLEEP")!=NULL){
@@ -105,11 +115,17 @@ char parser(char input[]){
         char *part1;
         char *part2;
         char *intmed2;
+        char *extra;
         func=strtok(exp, "(");
         intmed1=strtok(NULL, "(");
         part1=strtok(intmed1, ":");
         intmed2=strtok(NULL, ":");
         part2=strtok(intmed2, ")");
+        extra=strtok(NULL, ")");
+        if (*extra == '\0'){
+            op = 'q';
+            return 'q';
+        }
         printf("%s %s\n", part1, part2);
         // char* arr1[3];
         if (strstr(exp, "MIN")!=NULL){
