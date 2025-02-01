b33c16c64c66449a13af5abbd9f34a8bf9bf381d
Algorysh <154245420+Algorysh@users.noreply.github.com>
2025-02-01 20:53:38 +0000

    calculated and updated expressions

--- expcalc.c
+++ expcalc.c
@@ -11,25 +11,51 @@ void calc_value(cell *cell1) {  // Changed to pointer to modify the actual cell
     
     // Calculate count of elements
     count = (cell1->row2 - cell1->row1 + 1) * (cell1->col2 - cell1->col1 + 1);
+
+    if(cell1->operation == '+'){
+        val = mysheet[cell1->row1][cell1->col1]+mysheet[cell1->row2][cell1->col2];
+        cell1->sum = val;  //i will update sum and keep the init_value same while updating so that i can have both final_value and init_value and i don't have to make new variable for this
+    }
+
+    else if(cell1->operation == '-'){
+        val = mysheet[cell1->row1][cell1->col1]-mysheet[cell1->row2][cell1->col2];
+        cell1->sum = val;
+    }
+
+    else if(cell1->operation == '*'){
+        val = mysheet[cell1->row1][cell1->col1]*mysheet[cell1->row2][cell1->col2];
+        cell1->sum = val;
+    }
+
+    else if(cell1->operation == '/'){
+        val = mysheet[cell1->row1][cell1->col1]/mysheet[cell1->row2][cell1->col2];
+        cell1->sum = val;
+    }
     
     // Min operation
-    if(cell1->operation == 'm') {  // Changed to match struct comment
+    else if(cell1->operation == 'm') {
         val = INT_MAX;  // Initialize with maximum possible value
+        cell1.range_min_max = freeTree(cell1.range_min_max);
         for(int i = cell1->row1; i <= cell1->row2; i++) {
             for(int j = cell1->col1; j <= cell1->col2; j++) {
                 val = min(mysheet[i][j].value, val);
+                cell1.range_min_max = insert(cell1.range_min_max, mysheet[i][j].value);
             }
         }
+        cell1->sum = val;
     }
     
     // Max operation
     else if(cell1->operation == 'M') {
         val = INT_MIN;  // Initialize with minimum possible value
+        cell1.range_min_max = freeTree(cell1.range_min_max);
         for(int i = cell1->row1; i <= cell1->row2; i++) {
             for(int j = cell1->col1; j <= cell1->col2; j++) {
                 val = max(mysheet[i][j].value, val);
+                cell1.range_min_max = insert(cell1.range_min_max, mysheet[i][j].value);
             }
         }
+        cell1->sum = val;
     }
     
     // Sum operation
@@ -75,6 +101,88 @@ void calc_value(cell *cell1) {  // Changed to pointer to modify the actual cell
     }
     
     cell1->value = val;
+}
+
+void update_value(cell *cell1, short row, short col){
+    int new_val = 0;
+    int old_val = mysheet[row][col].value;
+
+    if (mysheet[row][col].operation == 'S') {
+        int count = (mysheet[row][col].row2 - mysheet[row][col].row1 + 1) * (mysheet[row][col].col2 - mysheet[row][col].col1 + 1);
+        double mean = (double)mysheet[row][col].sum / count;
+        double variance = ((double)mysheet[row][col].sq_sum / count) - (mean * mean);
+        double std_dev = sqrt(variance);
+        new_val = (int)std_dev;
+    } else if (mysheet[row][col].operation == 'a') {
+        int count = (mysheet[row][col].row2 - mysheet[row][col].row1 + 1) * (mysheet[row][col].col2 - mysheet[row][col].col1 + 1);
+        new_val = mysheet[row][col].sum / count;
+    } else {
+        new_val = mysheet[row][col].sum;
+    }
+
+
+    if (cell1->operation == '+') {
+        cell1->sum += (new_val - old_val);
+    } else if (cell1->operation == '-') {
+        if (cell1->row1 == row && cell1->col1 == col) {
+            cell1->sum += (new_val - old_val);
+        } else {
+            cell1->sum -= (new_val - old_val);
+        }
+    } else if (cell1->operation == '*') {
+        if (cell1->row1 == row && cell1->col1 == col) {
+            cell1->sum = new_val * (mysheet[cell1.row2][cell1.col2].sum);
+        } else {
+            cell1->sum = (mysheet[cell1.row1][cell1.col1].sum) * new_val;
+        }
+    } else if (cell1->operation == '/') {
+        if (cell1->row1 == row && cell1->col1 == col) {
+            cell1->sum = new_val / (mysheet[cell1.row2][cell1.col2].sum);
+        } else {
+            if (new_val != 0) {
+                cell1->sum = (mysheet[cell1.row1][cell1.col1].sum) / new_val;
+            } else {
+                cell1->sum = 0; // Avoid division by zero
+            }
+        }
+    } else if (cell1->operation == 'm') {
+        cell1.range_min_max = delete(cell1.range_min_max, old_val);
+        cell1.range_min_max = insert(cell1.range_min_max, new_val);
+        cell1.sum = (minValueNode(cell1.range_min_max))->value;
+    } else if (cell1->operation == 'M') {
+        cell1.range_min_max = delete(cell1.range_min_max, old_val);
+        cell1.range_min_max = insert(cell1.range_min_max, new_val);
+        cell1.sum = (maxValueNode(cell1.range_min_max))->value;
+    } else if (cell1->operation == 's') {
+        cell1->sum += (new_val - old_val);
+    } else if (cell1->operation == 'a') {
+        int count = (cell1->row2 - cell1->row1 + 1) * (cell1->col2 - cell1->col1 + 1);
+        cell1->sum += (new_val - old_val);
+        cell1->value = cell1->sum / count;
+    } else if (cell1->operation == 'S') {
+        int count = (cell1->row2 - cell1->row1 + 1) * (cell1->col2 - cell1->col1 + 1);
+        cell1->sum += (new_val - old_val);
+        cell1->sq_sum += (new_val * new_val - old_val * old_val);
+        double mean = (double)cell1->sum / count;
+        double variance = ((double)cell1->sq_sum / count) - (mean * mean);
+        cell1->value = (int)sqrt(variance);
+    }
+}
+
+void final_update(cell *cell1){
+    if(cell1.operation == 'S'){
+        int count = (cell1->row2 - cell1->row1 + 1) * (cell1->col2 - cell1->col1 + 1);
+        double mean = (double)cell1->sum / count;
+        double variance = ((double)cell1->sq_sum / count) - (mean * mean);
+        cell1->value = (int)sqrt(variance);
+    }
+    else if(cell1.operation == 'a'){
+        int count = (cell1->row2 - cell1->row1 + 1) * (cell1->col2 - cell1->col1 + 1);
+        cell1->value = cell1->sum / count;
+    }
+    else{
+        cell->value= cell->sum;
+    }
 }
 
 // int main(){
