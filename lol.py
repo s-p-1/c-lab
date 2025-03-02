7dee07b18aeb7d0db63a33382182389893afe3a2
Samarth Patel <146717917+s-p-1@users.noreply.github.com>
2025-03-02 13:46:52 +0000

    done

--- lol.py
+++ lol.py
@@ -1,5 +1,4 @@
 import string
-
 def int_to_str(n):
     result = []
     while n > 0:
@@ -9,7 +8,7 @@ def int_to_str(n):
     return ''.join(result[::-1])
 
 
-def generate_lines():
+def generate_lines_equal():
     lines = ["disable_output\n"]
     for j in range(1, 10):
         print(j)
@@ -20,7 +19,39 @@ def generate_lines():
     
     return lines
 
-lines = generate_lines()
+def generate_lines_Sum():
+    lines = ["disable_output"]
+    for j in range(1, 2):
+        print(j)
+        for i in range(1, 100):
+            lines.append(f'{int_to_str(j)}{i}=SUM({int_to_str(j)}{i+1}:ZZZ999)')
+        lines.append(f'{int_to_str(j)}999={int_to_str(j+1)}1')
+    lines.append("enable_output\nq\n")
+    
+    return lines
+
+def generate_lines_Max():
+    lines = ["disable_output"]
+    for j in range(1, 5):
+        print(j)
+        for i in range(1, 999):
+            lines.append(f'{int_to_str(j)}{i}=MAX({int_to_str(j)}{i+1}:ZZZ999)')
+        lines.append(f'{int_to_str(j)}999={int_to_str(j+1)}1')
+    lines.append("enable_output\nq\n")
+    
+    return lines
+
+def generate_small_sums():
+    lines = ["disable_output"]
+    for j in range(1, 2):
+        print(j)
+        for i in range(1, 100):
+            lines.append(f'{int_to_str(j)}{i}=SUM({int_to_str(j)}{i+1}:ZZ{i+100})')
+        lines.append(f'{int_to_str(j)}999={int_to_str(j+1)}1')
+    lines.append("enable_output\nq\n")
+    return lines
+
+lines = generate_lines_Max()
 with open('test.txt', 'w') as f:
     for line in lines:
         f.write(line + '\n')
